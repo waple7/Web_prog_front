9c793a102f78a39ce446301a62599d52cc66431c
Waple <naumenko.radmir@mail.ru>
2023-11-17 19:29:30 +0300

    chore: update naming and structure

--- js/script.js
+++ js/script.js
@@ -19,13 +19,30 @@
 // IIFE (Immediately Invoked Function Expression)-немедленно вызываемое функциональное выражение
 // обработчик события для window сработает когда все ресурсы будут загружены
 
-document.addEventListener('DOMContentLoaded', function() {
-    var currentPage = window.location.pathname;
-
-    var navLinks = document.querySelectorAll('a');
-    navLinks.forEach(function(link) {
-        if(link.getAttribute('href') === currentPage) {
-            link.parentElement.classList.add('active');
-        }
-    });
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     var currentPage = window.location.pathname;
+//
+//     var navLinks = document.querySelectorAll('a');
+//     navLinks.forEach(function(link) {
+//         if(link.getAttribute('href') === currentPage) {
+//             link.parentElement.classList.add('active');
+//         }
+//     });
+// });
+window.onload = function (){
+    if(document.location.href.includes("index")){
+        document.getElementsByClassName('button_main').item(0).classList.add("active");
+    }
+    if(document.location.href.includes("catalog")){
+        document.getElementsByClassName('button_catalog').item(0).classList.add("active");
+    }
+    if(document.location.href.includes("team")){
+        document.getElementsByClassName('button_team').item(0).classList.add("active");
+    }
+    if(document.location.href.includes("vacancies")){
+        document.getElementsByClassName('button_vacancies').item(0).classList.add("active");
+    }
+    if(document.location.href.includes("comments")){
+        document.getElementsByClassName('button_comments').item(0).classList.add("active");
+    }
+}
